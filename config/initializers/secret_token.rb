33f7f0e833f17255fcaf58ca7d44f26d142c3f7a
Kyle Selman <kselman@hotmail.co.uk>
2013-09-04 12:55:02 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GigApp::Application.config.secret_token = '408f81ac741c7f3807d1e2c733769fbff12ed375db1f1d583518ebee6e8f4fa8896b056e80ee945fe5f4b2341dae7c259829e3cc2809bbf5abe39b15864e1799'
