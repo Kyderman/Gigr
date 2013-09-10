6256b65238c661bfb1e9924642466abbb1800071
Kyle Selman <kselman@hotmail.co.uk>
2013-09-10 16:58:23 +0100

    end of day 2

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :description, :name, :venue_id, :band_ids
+  attr_accessible :start, :end,  :description, :name, :venue_id, :band_ids
   has_many :bands_event
   belongs_to :venue
   has_many :bands, through: :bands_event
   accepts_nested_attributes_for :bands, :bands_event
   
   def start_time
-    date
+    start
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130910152253) do
+ActiveRecord::Schema.define(:version => 20130910154622) do
 
   create_table "bands", :force => true do |t|
     t.string   "name"
@@ -47,9 +47,10 @@ ActiveRecord::Schema.define(:version => 20130910152253) do
     t.string   "name"
     t.string   "description"
     t.integer  "venue_id"
-    t.datetime "date"
+    t.datetime "start"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.datetime "end"
   end
 
   create_table "musicians", :force => true do |t|
